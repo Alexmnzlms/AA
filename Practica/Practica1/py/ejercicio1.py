8fc1761dc1431ce854c0f05f7930b80f71f30474
Alexmnzlms <alexmanza2@gmail.com>
2020-03-25 11:14:47 +0100

    Gradiente II

--- Practica/Practica1/py/ejercicio1.py
+++ Practica/Practica1/py/ejercicio1.py
@@ -35,14 +35,16 @@ def gradient_descent(w,n,iterations,min):
     u = w[0]
     v = w[1]
 
-    while E(u,v) > min or i < iterations:
+    while E(u,v) > min and i < iterations:
         d = gradE(u,v);
         u = u - 0.1*dEu(u,v)
         v = v - 0.1*dEv(u,v)
         i = i + 1
-        print('E(',u,',',v,'): ', E(u,v), ',' ,i,'\n')
+        print('E(',u,',',v,'): ', E(u,v), ',' ,iterations-i)
 
-    print(E(u,v))
+    w[0] = u
+    w[1] = v
+
     return w, iterations
 
 
@@ -58,22 +60,22 @@ print ('Coordenadas obtenidas: (', w[0], ', ', w[1],')')
 
 
 # DISPLAY FIGURE
-from mpl_toolkits.mplot3d import Axes3D
-x = np.linspace(-30, 30, 50)
-y = np.linspace(-30, 30, 50)
-X, Y = np.meshgrid(x, y)
-Z = E(X, Y) #E_w([X, Y])
-fig = plt.figure()
-ax = Axes3D(fig)
-surf = ax.plot_surface(X, Y, Z, edgecolor='none', rstride=1,
-                        cstride=1, cmap='jet')
-min_point = np.array([w[0],w[1]])
-min_point_ = min_point[:, np.newaxis]
-ax.plot(min_point_[0], min_point_[1], E(min_point_[0], min_point_[1]), 'r*', markersize=10)
-ax.set(title='Ejercicio 1.2. Función sobre la que se calcula el descenso de gradiente')
-ax.set_xlabel('u')
-ax.set_ylabel('v')
-ax.set_zlabel('E(u,v)')
+#from mpl_toolkits.mplot3d import Axes3D
+#x = np.linspace(-30, 30, 50)
+#y = np.linspace(-30, 30, 50)
+#X, Y = np.meshgrid(x, y)
+#Z = E(X, Y) #E_w([X, Y])
+#fig = plt.figure()
+#ax = Axes3D(fig)
+#surf = ax.plot_surface(X, Y, Z, edgecolor='none', rstride=1,
+#                        cstride=1, cmap='jet')
+#min_point = np.array([w[0],w[1]])
+#min_point_ = min_point[:, np.newaxis]
+#ax.plot(min_point_[0], min_point_[1], E(min_point_[0], min_point_[1]), 'r*', markersize=10)
+#ax.set(title='Ejercicio 1.2. Función sobre la que se calcula el descenso de gradiente')
+#ax.set_xlabel('u')
+#ax.set_ylabel('v')
+#ax.set_zlabel('E(u,v)')
 
 input("\n--- Pulsar tecla para continuar ---\n")
 
