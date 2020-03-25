42dbecb41ab80f910f8dc47e3695194a3c91f8bf
Alexmnzlms <alexmanza2@gmail.com>
2020-03-25 10:25:35 +0100

    Gradiente

--- Practica/Practica1/py/ejercicio1.py
+++ Practica/Practica1/py/ejercicio1.py
@@ -7,6 +7,7 @@ Nombre Estudiante: Alejandro Manzanares Lemus
 import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sym
+import math
 
 np.random.seed(1)
 
@@ -14,26 +15,34 @@ print('EJERCICIO SOBRE LA BUSQUEDA ITERATIVA DE OPTIMOS\n')
 print('Ejercicio 1\n')
 
 def E(u,v):
-    return np.float64(u*math.e**v - 2*v*math.e**(u*-1))**2)
+    return np.float((u*math.e**v - 2*v*math.e**(u*-1))**2)
 
 #Derivada parcial de E con respecto a u
 def dEu(u,v):
-    x,y = sp.Symbol('u v')
-    return np.flota64(sym.diff((u*math.e**v - 2*v*math.e**(u*-1))**2,x))
+    return np.float(2*(u*math.e**v - 2*v*math.e**(u*-1))*(math.e**v + 2*v*math.e**(u*-1)))
 
 #Derivada parcial de E con respecto a v
 def dEv(u,v):
-    x,y = sp.Symbol('u v')
-    return np.flota64(sym.diff((u*math.e**v - 2*v*math.e**(u*-1))**2,y))
+    return np.float(2*(u*math.e**v - 2*v*math.e**(u*-1))*(u*math.e**v - 2*math.e**(u*-1)))
 
 #Gradiente de E
 def gradE(u,v):
     return np.array([dEu(u,v), dEv(u,v)])
 
-def gradient_descent(?):
-    #
-    # gradiente descendente
-    #
+def gradient_descent(w,n,iterations,min):
+    print('Funcion gradiente para: ', min, ' -> ',iterations)
+    i = 0
+    u = w[0]
+    v = w[1]
+
+    while E(u,v) > min or i < iterations:
+        d = gradE(u,v);
+        u = u - 0.1*dEu(u,v)
+        v = v - 0.1*dEv(u,v)
+        i = i + 1
+        print('E(',u,',',v,'): ', E(u,v), ',' ,i,'\n')
+
+    print(E(u,v))
     return w, iterations
 
 
@@ -41,7 +50,7 @@ eta = 0.01
 maxIter = 10000000000
 error2get = 1e-14
 initial_point = np.array([1.0,1.0])
-w, it = gradient_descent(?)
+w, it = gradient_descent(initial_point,eta,maxIter,error2get);
 
 
 print ('Numero de iteraciones: ', it)
