e63b0f16bd7d8cf1a3e9720fe2d7d7bbfe4093db
Alexmnzlms <alexmanza2@gmail.com>
2020-04-26 19:46:17 +0200

    Cambios entrega

--- Practica/Practica2/generar_entrega.sh
+++ Practica/Practica2/generar_entrega.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+cd doc
+make
+make
+cd ..
 cp doc/demo.pdf .
 mv demo.pdf Memoria_Practica2_Alejandro_Manzanares_Lemus.pdf
 cp py/* .
 zip -r practica2_manzanares_lemus_alejandro.zip ./*.py ./Memoria_Practica2_Alejandro_Manzanares_Lemus.pdf
-rm Memoria_Practica2_Alejandro_Manzanares_Lemus.pdf
 rm *.py
