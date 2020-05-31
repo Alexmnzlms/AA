62fd8d07fc4723f9b1f2e66d9faa59de08b2ae4d
Alexmnzlms <alexmanza2@gmail.com>
2020-05-31 07:38:53 +0200

    Final final

--- Practica/Practica3/generar_entrega.sh
+++ Practica/Practica3/generar_entrega.sh
@@ -5,4 +5,4 @@ make
 cd ..
 cp doc/demo.pdf .
 mv demo.pdf Memoria_Practica3_Alejandro_Manzanares_Lemus.pdf
-zip -r practica2_manzanares_lemus_alejandro.zip ./*.py ./Memoria_Practica3_Alejandro_Manzanares_Lemus.pdf
+zip -r practica3_manzanares_lemus_alejandro.zip ./*.py ./Memoria_Practica3_Alejandro_Manzanares_Lemus.pdf
